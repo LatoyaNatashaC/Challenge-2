95eaf1192ef047f267e4c4a92f4048136ca463f9
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-06-08 00:56:48 -0700

    enable day and night theme toggle

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hallo");
+const html = document.getElementById("htmlPage");
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    html.setAttribute("data-bs-theme", "dark");
+  } else {
+    html.setAttribute("data-bs-theme", "light");
+  }
+});
